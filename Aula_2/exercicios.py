688eda56e2d259167b5406082b58b1ec008891ab
Rodrigo <rsvitorrs@gmail.com>
2024-08-31 14:57:38 -0300

    execercicios resolvidos

---
+++ Aula_2/exercicios.py
@@ -0,0 +1,38 @@
+#Exercícios
+#Inteiros (int)
+
+#1 Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+#valor1 = int(input("Insira um numero: "))
+#valor2 = int(input("Insira outro numero: "))
+#calc_valores = (valor1 + valor2) 
+#print(f"O resultado do calculo é: {calc_valores}")
+
+#2 Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+#valor1 = int(input("Insira um numero: "))
+#valor2 = int(5)
+#calc_valores = (valor1 % valor2) # % é usado para calcular o resto da divisão
+#print(f"O resto da divisão por 5 é: {calc_valores}")
+
+#3 Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+#valor1 = int(input("Digite um numero: "))
+#valor2 = int(input("Digite outro numero: "))
+#calc_valores = valor1 * valor2
+#print("O resultado da mutiplicação é: ",calc_valores)
+
+#4 Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+#valor1 = int(input("Digite um numero: "))
+#valor2 = int(input("Digite outro numero: "))
+#calc_valores = valor1 / valor2
+#print("O resultado da divisão é: ", int(calc_valores)) # Usei conversão direta para não devolver um valor float -> ex: 7.0
+
+#5 Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+#valor = int(input("Digite um numero: "))
+#calc_potencia = valor ** 2
+#calc_potencia = pow(valor,2)
+#print(f"O valor de {valor} eleveado ao quadrado é: {calc_potencia}")
+
